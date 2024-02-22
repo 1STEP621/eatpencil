988550bf48fdc0779830c3dc940b29f6ad4493c3
1Step621 <onestep621@gmail.com>
2024-02-22 10:34:23 +0900

    wip: note action button

--- lib/components/note.dart
+++ lib/components/note.dart
@@ -1,7 +1,10 @@
+import 'package:eatpencil/components/space.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tabler_icons_for_flutter/tabler_icons_for_flutter.dart';
 import 'package:misskey_dart/misskey_dart.dart';
 import 'package:mfm/mfm.dart';
+import 'package:eatpencil/providers.dart';
 
 class NoteCard extends ConsumerWidget {
   final Note note;
@@ -15,13 +18,69 @@ class NoteCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(note.user.name ?? "",
-                style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              note.user.name ?? "",
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
             Mfm(mfmText: note.text ?? ""),
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(TablerIcons.arrow_back_up),
+                  onPressed: () {},
+                  iconSize: 20,
+                  visualDensity: const VisualDensity(
+                    horizontal: VisualDensity.minimumDensity,
+                    vertical: VisualDensity.minimumDensity,
+                  ),
+                  padding: EdgeInsets.zero,
+                  color: theme(ref).fg,
+                ),
+                const Space(width: 20),
+                IconButton(
+                  icon: const Icon(TablerIcons.repeat),
+                  onPressed: () {},
+                  iconSize: 20,
+                  visualDensity: const VisualDensity(
+                    horizontal: VisualDensity.minimumDensity,
+                    vertical: VisualDensity.minimumDensity,
+                  ),
+                  padding: EdgeInsets.zero,
+                  color: theme(ref).fg,
+                ),
+                const Space(width: 20),
+                IconButton(
+                  icon: const Icon(TablerIcons.plus),
+                  onPressed: () {},
+                  iconSize: 20,
+                  visualDensity: const VisualDensity(
+                    horizontal: VisualDensity.minimumDensity,
+                    vertical: VisualDensity.minimumDensity,
+                  ),
+                  padding: EdgeInsets.zero,
+                  color: theme(ref).fg,
+                ),
+                const Space(width: 20),
+                IconButton(
+                  icon: const Icon(TablerIcons.dots),
+                  onPressed: () {},
+                  iconSize: 20,
+                  visualDensity: const VisualDensity(
+                    horizontal: VisualDensity.minimumDensity,
+                    vertical: VisualDensity.minimumDensity,
+                  ),
+                  alignment: Alignment.center,
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(const CircleBorder()),
+                  ),
+                  color: theme(ref).fg,
+                ),
+              ],
+            )
           ],
         ),
       ),
