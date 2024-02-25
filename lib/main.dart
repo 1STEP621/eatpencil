d3d92541a9d11a1852fbf5e70d49fa33814197c2
1Step621 <onestep621@gmail.com>
2024-02-25 23:17:10 +0900

    fix: revert transparent bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:eatpencil/providers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:eatpencil/router.dart';
 
@@ -12,12 +11,6 @@ class Eatpencil extends ConsumerWidget {
   const Eatpencil({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: [SystemUiOverlay.top]).then((_) {
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-        systemNavigationBarColor: Colors.transparent,
-        systemNavigationBarDividerColor: Colors.transparent,
-      ));
-    });
     return MaterialApp.router(
       title: 'Eatpencil',
       routerDelegate: goRouter.routerDelegate,
