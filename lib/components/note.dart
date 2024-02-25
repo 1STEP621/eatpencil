dfb7888a163d841e1e0c2f26247717ebf88d4747
1Step621 <onestep621@gmail.com>
2024-02-26 01:49:59 +0900

    refactor: format

--- lib/components/note.dart
+++ lib/components/note.dart
@@ -34,135 +34,136 @@ class NoteCard extends ConsumerWidget {
 
     return Card(
       child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: isPureRenote
-              ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RenoterIndicator(note: note),
-                    const Space(height: 10),
-                    NoteCard(
-                      note: note.renote!,
-                      server: server,
+        padding: const EdgeInsets.all(15),
+        child: isPureRenote
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RenoterIndicator(note: note),
+                  const Space(height: 10),
+                  NoteCard(
+                    note: note.renote!,
+                    server: server,
+                  ),
+                ],
+              )
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    note.user.name ?? "",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  ],
-                )
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      note.user.name ?? "",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        overflow: TextOverflow.ellipsis,
+                  ),
+                  Mfm(
+                    mfmText: note.text ?? "",
+                  ),
+                  GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2,
+                    childAspectRatio: 16 / 9,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      for (final file in note.files)
+                        Image.network(
+                          file.thumbnailUrl ?? file.url,
+                          fit: BoxFit.contain,
+                        ),
+                    ],
+                  ),
+                  if (isQuote && (depth ?? 0) < 4)
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: theme(ref).renote,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: NoteCard(
+                        note: note.renote!,
+                        server: server,
+                        depth: (depth ?? 0) + 1,
                       ),
                     ),
-                    Mfm(
-                      mfmText: note.text ?? "",
-                    ),
-                    GridView.count(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 2,
-                      mainAxisSpacing: 2,
-                      childAspectRatio: 16 / 9,
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: [
-                        for (final file in note.files)
-                          Image.network(
-                            file.thumbnailUrl ?? file.url,
-                            fit: BoxFit.contain,
+                  if ((depth ?? 0) < 1)
+                    ReactionsViewer(
+                      note: note,
+                      onReactionTap: (reaction) {
+                        server.notes.reactions.create(
+                          NotesReactionsCreateRequest(
+                            noteId: note.id,
+                            reaction: reaction,
                           ),
+                        );
+                      },
+                    ),
+                  if ((depth ?? 0) < 1)
+                    Row(
+                      children: [
+                        SimpleIconButton(
+                          icon: const Icon(TablerIcons.arrow_back_up),
+                          onPressed: () {
+                            // TODO: Reply
+                          },
+                        ),
+                        const Space(width: 20),
+                        SimpleIconButton(
+                          icon: const Icon(TablerIcons.repeat),
+                          onPressed: () {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return BottomSheetMenu(
+                                  entries: [
+                                    Entry(
+                                      title: "リノート",
+                                      icon: TablerIcons.repeat,
+                                      onPressed: () {
+                                        server.notes.create(
+                                          NotesCreateRequest(
+                                            renoteId: note.id,
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                    Entry(
+                                      title: "引用リノート",
+                                      icon: TablerIcons.quote,
+                                      onPressed: () {
+                                        // TODO: Quote
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                        const Space(width: 20),
+                        SimpleIconButton(
+                          icon: const Icon(TablerIcons.plus),
+                          onPressed: () {
+                            // TODO: Reaction
+                          },
+                        ),
+                        const Space(width: 20),
+                        SimpleIconButton(
+                          icon: const Icon(TablerIcons.dots),
+                          onPressed: () {
+                            // TODO: Context menu
+                          },
+                        ),
                       ],
                     ),
-                    if (isQuote && (depth ?? 0) < 4)
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: theme(ref).renote,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: NoteCard(
-                          note: note.renote!,
-                          server: server,
-                          depth: (depth ?? 0) + 1,
-                        ),
-                      ),
-                    if ((depth ?? 0) < 1)
-                      ReactionsViewer(
-                        note: note,
-                        onReactionTap: (reaction) {
-                          server.notes.reactions.create(
-                            NotesReactionsCreateRequest(
-                              noteId: note.id,
-                              reaction: reaction,
-                            ),
-                          );
-                        },
-                      ),
-                    if ((depth ?? 0) < 1)
-                      Row(
-                        children: [
-                          SimpleIconButton(
-                            icon: const Icon(TablerIcons.arrow_back_up),
-                            onPressed: () {
-                              // TODO: Reply
-                            },
-                          ),
-                          const Space(width: 20),
-                          SimpleIconButton(
-                            icon: const Icon(TablerIcons.repeat),
-                            onPressed: () {
-                              showModalBottomSheet(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return BottomSheetMenu(
-                                    entries: [
-                                      Entry(
-                                        title: "リノート",
-                                        icon: TablerIcons.repeat,
-                                        onPressed: () {
-                                          server.notes.create(
-                                            NotesCreateRequest(
-                                              renoteId: note.id,
-                                            ),
-                                          );
-                                        },
-                                      ),
-                                      Entry(
-                                        title: "引用リノート",
-                                        icon: TablerIcons.quote,
-                                        onPressed: () {
-                                          // TODO: Quote
-                                        },
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                          ),
-                          const Space(width: 20),
-                          SimpleIconButton(
-                            icon: const Icon(TablerIcons.plus),
-                            onPressed: () {
-                              // TODO: Reaction
-                            },
-                          ),
-                          const Space(width: 20),
-                          SimpleIconButton(
-                            icon: const Icon(TablerIcons.dots),
-                            onPressed: () {
-                              // TODO: Context menu
-                            },
-                          ),
-                        ],
-                      ),
-                  ],
-                )),
+                ],
+              ),
+      ),
     );
   }
 }
