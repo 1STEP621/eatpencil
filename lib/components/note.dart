874f260aaf877d70933c7a6ad2577d8388483db7
1Step621 <onestep621@gmail.com>
2024-03-01 23:48:51 +0900

    refactor: format

--- lib/components/note.dart
+++ lib/components/note.dart
@@ -18,16 +18,17 @@ class NoteCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       decoration: BoxDecoration(
-          color: theme(ref).panel,
-          // 一番上と一番下に余計なBorderがつくので、
-          // https://github.com/flutter/flutter/issues/48226
-          // が実装され次第修正
-          border: Border.symmetric(
-            horizontal: BorderSide(
-              color: theme(ref).divider,
-              width: 0.5,
-            ),
-          )),
+        color: theme(ref).panel,
+        // 一番上と一番下に余計なBorderがつくので、
+        // https://github.com/flutter/flutter/issues/48226
+        // が実装され次第修正
+        border: Border.symmetric(
+          horizontal: BorderSide(
+            color: theme(ref).divider,
+            width: 0.5,
+          ),
+        ),
+      ),
       child: Padding(
         padding: const EdgeInsets.all(12.5),
         child: NoteContent(
